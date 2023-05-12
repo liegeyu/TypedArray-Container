852cd80944c01c9678e686deb60a9b499e9ab02c
hairy377 <480420979@qq.com>
2023-05-12 23:18:35 +0800

    dictionary

--- typedArrayList.js
+++ typedArrayList.js
@@ -1,10 +1,17 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TypedArrayList = void 0;
-// TypedArrayList
-var TypedArrayList = /** @class */ (function () {
-    function TypedArrayList(typedArrayConstructor, capacity) {
-        if (capacity === void 0) { capacity = 8; }
+/**
+ * @类 TypedArrayList
+ * 类型数组链表
+ * @get length: number  获取 typedArray 的长度
+ * @get capacity: number  获取开辟的 typedArray 的长度
+ * @get typedArray: number  获取 typedArray
+ * @function clear
+ * @function find
+ * @function push
+ * @function slice
+ * @function subArray
+ */
+export class TypedArrayList {
+    constructor(typedArrayConstructor, capacity = 8) {
         // 扩容 callback
         this.capacityChangedCallback = null;
         this._typedArrayConstructor = typedArrayConstructor;
@@ -15,46 +22,34 @@ var TypedArrayList = /** @class */ (function () {
         this._array = new this._typedArrayConstructor(this._capacity);
         this._length = 0;
     }
-    Object.defineProperty(TypedArrayList.prototype, "length", {
-        get: function () {
-            return this._length;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(TypedArrayList.prototype, "capacity", {
-        get: function () {
-            return this._capacity;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(TypedArrayList.prototype, "typedArray", {
-        get: function () {
-            return this._array;
-        },
-        enumerable: false,
-        configurable: true
-    });
+    get length() {
+        return this._length;
+    }
+    get capacity() {
+        return this._capacity;
+    }
+    get typedArray() {
+        return this._array;
+    }
     // clear 方法
-    TypedArrayList.prototype.clear = function () {
+    clear() {
         this._length = 0;
-    };
-    // get 方法
-    TypedArrayList.prototype.get = function (index) {
+    }
+    // find 方法
+    find(index) {
         if (index < 0 || index >= this._length) {
             throw new Error("index overshoot!");
         }
         return this._array[index];
-    };
+    }
     // push 方法
-    TypedArrayList.prototype.push = function (num) {
+    push(num) {
         // length > capacity 扩容
         if (this._length >= this._capacity) {
             if (this._capacity > 0) {
                 this._capacity += this._capacity;
             }
-            var oldArray = this._array;
+            let oldArray = this._array;
             this._array = new this._typedArrayConstructor(this._capacity);
             this._array.set(oldArray);
             if (this.capacityChangedCallback !== null) {
@@ -63,30 +58,134 @@ var TypedArrayList = /** @class */ (function () {
         }
         this._array[this._length++] = num;
         return this._length;
-    };
+    }
     // subArray 方法
-    TypedArrayList.prototype.subArray = function (start, end) {
-        if (start === void 0) { start = 0; }
-        if (end === void 0) { end = this._length; }
+    subArray(start = 0, end = this._length) {
         return this._array.subarray(start, end);
-    };
+    }
     // slice 方法
-    TypedArrayList.prototype.slice = function (start, end) {
-        if (start === void 0) { start = 0; }
-        if (end === void 0) { end = this._length; }
+    slice(start = 0, end = this._length) {
         return this._array.slice(start, end);
-    };
-    return TypedArrayList;
-}());
-exports.TypedArrayList = TypedArrayList;
+    }
+}
+/**
+ * @类 Dictionary
+ * 字典对象, Dictionary(useMap: boolean = true) useMap 为 true 则使用 Map<>() 结构, 否则使用 { [key: string]: T } 索引签名
+ * @get length: number  获取 Dictionary 长度
+ * @get keys: string[]  获取所有键 key
+ * @get values: T[]  获取所有值 value
+ * @function contains
+ * @function find
+ * @function insert
+ * @function remove
+ * @function toString
+ */
+export class Dictionary {
+    constructor(useMap = true) {
+        this._count = 0;
+        if (useMap) {
+            this._items = new Map();
+        }
+        else {
+            this._items = {};
+        }
+    }
+    get length() {
+        return this._count;
+    }
+    get keys() {
+        let keys = [];
+        if (this._items instanceof Map) {
+            let keyArr = this._items.keys();
+            for (let key of keyArr) {
+                keys.push(key);
+            }
+        }
+        else {
+            for (let prop in this._items) {
+                if (this._items.hasOwnProperty(prop)) {
+                    keys.push(prop);
+                }
+            }
+        }
+        return keys;
+    }
+    get values() {
+        let values = [];
+        if (this._items instanceof Map) {
+            let valArr = this._items.values();
+            for (let val of valArr) {
+                values.push(val);
+            }
+        }
+        else {
+            for (let prop in this._items) {
+                if (this._items.hasOwnProperty(prop)) {
+                    values.push(this._items[prop]);
+                }
+            }
+        }
+        return values;
+    }
+    // 判断 key 是否存在
+    contains(key) {
+        if (this._items instanceof Map) {
+            return this._items.has(key);
+        }
+        else {
+            return this._items[key] !== undefined;
+        }
+    }
+    // 查询值
+    find(key) {
+        if (this._items instanceof Map) {
+            return this._items.get(key);
+        }
+        else {
+            return this._items[key];
+        }
+    }
+    // 插入一个键值对
+    insert(key, value) {
+        if (this._items instanceof Map) {
+            this._items.set(key, value);
+        }
+        else {
+            this._items[key] = value;
+        }
+        this._count++;
+    }
+    // 删除
+    remove(key) {
+        const ret = this.find(key);
+        if (ret === undefined) {
+            return false;
+        }
+        if (this._items instanceof Map) {
+            this._items.delete(key);
+        }
+        else {
+            delete this._items[key];
+        }
+        this._count--;
+        return false;
+    }
+    // toString 方法
+    toString() {
+        return JSON.stringify(this._items);
+    }
+}
+/**
+ * 用例 ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+ */
 // init TypedArrayList
-var fload32ArrayList = new TypedArrayList(Float32Array);
-var uint16Array = new TypedArrayList(Uint16Array);
+let fload32ArrayList = new TypedArrayList(Float32Array);
+let uint16Array = new TypedArrayList(Uint16Array);
 console.log("old", fload32ArrayList);
 // testSubarray
 function testSubarray(subarray) {
-    var f32arr = new Uint16Array([0, 1, 2, 3, 4]);
-    var subrange;
+    let f32arr = new Uint16Array([0, 1, 2, 3, 4]);
+    let subrange;
     if (subarray === true) {
         subrange = f32arr.subarray(1, 3);
     }
@@ -97,6 +196,22 @@ function testSubarray(subarray) {
 }
 testSubarray(true);
 testSubarray(false);
-// map
-var map = new Map();
-console.log(map);
+// Dictionary
+let dict = new Dictionary(false);
+// insert
+dict.insert("a", "a");
+dict.insert("b", "b");
+dict.insert("c", "c");
+dict.insert("d", "d");
+console.log("insert", JSON.stringify(dict));
+// remove
+dict.remove("c");
+console.log("remove", JSON.stringify(dict));
+// contains
+console.log("contains", dict.contains("c"));
+// find
+console.log("find", dict.find("b"));
+// keys
+console.log("keys", dict.keys);
+// values
+console.log("values", dict.values);
--- typedArrayList.ts
+++ typedArrayList.ts
@@ -1,11 +1,14 @@
 /**
  * @类 TypedArrayList
  * 类型数组链表
- * @clear
- * @get
- * @push
- * @slice
- * @subArray
+ * @get length: number  获取 typedArray 的长度
+ * @get capacity: number  获取开辟的 typedArray 的长度
+ * @get typedArray: number  获取 typedArray
+ * @function clear
+ * @function find
+ * @function push
+ * @function slice
+ * @function subArray
  */
 export class TypedArrayList<T extends Uint16Array | Uint8Array | Float32Array> {
   private _array: T;
@@ -49,8 +52,8 @@ export class TypedArrayList<T extends Uint16Array | Uint8Array | Float32Array> {
     this._length = 0;
   }
 
-  // get 方法
-  public get(index: number): number {
+  // find 方法
+  public find(index: number): number {
     if (index < 0 || index >= this._length) {
       throw new Error("index overshoot!");
     }
@@ -92,9 +95,233 @@ export class TypedArrayList<T extends Uint16Array | Uint8Array | Float32Array> {
 
 /**
  * @类 Dictionary
- * 字典对象
+ * 字典对象, Dictionary(useMap: boolean = true) useMap 为 true 则使用 Map<>() 结构, 否则使用 { [key: string]: T } 索引签名
+ * @get length: number  获取 Dictionary 长度
+ * @get keys: string[]  获取所有键 key
+ * @get values: T[]  获取所有值 value
+ * @function contains
+ * @function find
+ * @function insert
+ * @function remove
+ * @function toString
  */
-export class Dictionary<T> {}
+export class Dictionary<T> {
+  private _items: { [key: string]: T } | Map<string, T>;
+  private _count: number = 0;
+
+  public constructor(useMap: boolean = true) {
+    if (useMap) {
+      this._items = new Map<string, T>();
+    } else {
+      this._items = {};
+    }
+  }
+
+  public get length(): number {
+    return this._count;
+  }
+
+  public get keys(): string[] {
+    let keys: string[] = [];
+
+    if (this._items instanceof Map) {
+      let keyArr = this._items.keys();
+      for (let key of keyArr) {
+        keys.push(key);
+      }
+    } else {
+      for (let prop in this._items) {
+        if (this._items.hasOwnProperty(prop)) {
+          keys.push(prop);
+        }
+      }
+    }
+
+    return keys;
+  }
+
+  public get values(): T[] {
+    let values: T[] = [];
+
+    if (this._items instanceof Map) {
+      let valArr = this._items.values();
+      for (let val of valArr) {
+        values.push(val);
+      }
+    } else {
+      for (let prop in this._items) {
+        if (this._items.hasOwnProperty(prop)) {
+          values.push(this._items[prop]);
+        }
+      }
+    }
+
+    return values;
+  }
+
+  // 判断 key 是否存在
+  public contains(key: string): boolean {
+    if (this._items instanceof Map) {
+      return this._items.has(key);
+    } else {
+      return this._items[key] !== undefined;
+    }
+  }
+
+  // 查询值
+  public find(key: string): T | undefined {
+    if (this._items instanceof Map) {
+      return this._items.get(key);
+    } else {
+      return this._items[key];
+    }
+  }
+
+  // 插入一个键值对
+  public insert(key: string, value: T): void {
+    if (this._items instanceof Map) {
+      this._items.set(key, value);
+    } else {
+      this._items[key] = value;
+    }
+
+    this._count++;
+  }
+
+  // 删除
+  public remove(key: string): boolean {
+    const ret = this.find(key);
+    if (ret === undefined) {
+      return false;
+    }
+
+    if (this._items instanceof Map) {
+      this._items.delete(key);
+    } else {
+      delete this._items[key];
+    }
+
+    this._count--;
+    return false;
+  }
+
+  // toString 方法
+  public toString(): string {
+    return JSON.stringify(this._items as Map<string, T>);
+  }
+}
+
+/**
+ * @类 ListNode
+ * 双向循环链表节点
+ */
+export class ListNode<T> {
+  public next: ListNode<T> | null;
+  public prev: ListNode<T> | null;
+  public data: T | undefined;
+
+  public constructor(data: T | undefined = undefined) {
+    this.next = null;
+    this.prev = null;
+    this.data = data;
+  }
+}
+
+/**
+ * 类 List
+ * 双向循环链表
+ */
+export class List<T> {
+  private _headNode: ListNode<T>;
+  private _length: number;
+
+  public constructor() {
+    this._headNode = new ListNode<T>();
+    this._headNode.next = this._headNode;
+    this._headNode.prev = this._headNode;
+    this._length = 0;
+  }
+
+  // 获取 List 的长度
+  public get length(): number {
+    return this._length;
+  }
+
+  // 判断是否为空链表
+  public empty(): boolean {
+    return this._headNode.next === this._headNode;
+  }
+
+  public begin(): ListNode<T> {
+    if (this._headNode.next === null) {
+      throw new Error("头节点");
+    }
+
+    return this._headNode.next;
+  }
+
+  public end(): ListNode<T> {
+    return this._headNode;
+  }
+
+  // 查询是否存在某值
+  public contains(data: T): boolean {
+    for (
+      let node: ListNode<T> | null = this._headNode.next;
+      node !== null && node !== this._headNode;
+      node = node.next
+    ) {
+      if (node.data !== undefined) {
+        if (data === node.data) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  // 插入
+  public insert(node: ListNode<T>, data: T): number {
+    let ret = new ListNode<T>(data);
+    ret.next = node;
+    ret.prev = node.prev;
+
+    if (node.prev !== null) {
+      node.prev.next = ret;
+    }
+    node.prev = ret;
+    this._length++;
+
+    return this._length;
+  }
+
+  // next 遍历并执行回调
+  public forNext(cb: (data: T) => void): void {
+    for (
+      let node: ListNode<T> | null = this._headNode.next;
+      node !== null && node !== this._headNode;
+      node = node.next
+    ) {
+      if (node.data !== undefined) {
+        cb(node.data);
+      }
+    }
+  }
+
+  // prev 遍历并执行回调
+  public forPrev(cb: (data: T) => void): void {
+    for (
+      let node: ListNode<T> | null = this._headNode.next;
+      node !== null && node !== this._headNode;
+      node = node.prev
+    ) {
+      if (node.data !== undefined) {
+        cb(node.data);
+      }
+    }
+  }
+}
 
 /**
  * 用例 ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
@@ -123,3 +350,28 @@ function testSubarray(subarray: boolean): void {
 
 testSubarray(true);
 testSubarray(false);
+
+// Dictionary
+let dict: Dictionary<string> = new Dictionary(false);
+// insert
+dict.insert("a", "a");
+dict.insert("b", "b");
+dict.insert("c", "c");
+dict.insert("d", "d");
+console.log("insert", JSON.stringify(dict));
+
+// remove
+dict.remove("c");
+console.log("remove", JSON.stringify(dict));
+
+// contains
+console.log("contains", dict.contains("c"));
+
+// find
+console.log("find", dict.find("b"));
+
+// keys
+console.log("keys", dict.keys);
+
+// values
+console.log("values", dict.values);
